c8a4f0e57339f6dd2d948a3b8a506383411a72e1
smb2255 <smb2255@columbia.edu>
2018-03-08 19:07:13 -0500

    image - added an image with img src.....direct link from imgur

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,12 +2,10 @@ const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 const fillers = require('./fillers/events')
 const users = require('./users/events')
-const checkout = require('./checkout/events')
 
 $(() => {
   setAPIOrigin(location, config)
   fillers.onPageLoad()
   fillers.eventListeners()
   users.userEventListeners()
-  checkout.onCheckout()
 })
